4cfcf4021afeea8ff2d537c9aad3cc2c2fcb0498
Khalid Fazal <khalidahmedfazal01@gmail.com>
2022-02-23 16:57:55 +0300

    Header almost done

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -10,12 +10,14 @@ export default function Header() {
             </div>
             
             <nav>
-
+                <a href="#about">about</a>
+                <a href="#experience">experience</a>
+                <a href="#mywork">my work</a>
+                <a href="#contactme">contact me</a>
             </nav>
             
             <div className="header_resume">
                 <div>
-                    {/* <span>resume</span> */}
                     <a href="/">resume</a>
                 </div>
             </div>
@@ -25,8 +27,36 @@ export default function Header() {
                 <span></span>
             </div>
             
-            <div className="header_drawer">
+            <div className="header_drawer open">
+                <div className="header_drawer_top">
+                    <a href="/"><Logo/></a>
+            
+                    <div className="header_drawer_top_close">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                
+                <div className="header_drawer_bottom">
+                    <div className="header_drawer_bottom_socials">
+                        
+                    </div>
 
+                    <div className="header_drawer_bottom_menu">
+                        <nav>
+                            <a href="#about">about</a>
+                            <a href="#experience">experience</a>
+                            <a href="#mywork">my work</a>
+                            <a href="#contactme">contact me</a>
+                        </nav>
+                    
+                        <div className="header_drawer_bottom_menu_resume">
+                            <div>
+                                <a href="/">resume</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
     );
