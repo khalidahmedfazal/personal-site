58d79ceb54ee732ba69a8aeba1e9b5f164d61298
Khalid Fazal <khalidahmedfazal01@gmail.com>
2022-03-03 09:33:29 +0300

    Removed server

---
+++ src/components/header.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import $ from 'jquery';
+
+import '../styles/header.css';
+
+import { ReactComponent as LogoDarkSVG } from '../assets/logo-dark.svg';
+import { ReactComponent as LogoLightSVG } from '../assets/logo-light.svg';
+import { ReactComponent as MoonSVG } from '../assets/moon.svg';
+import { ReactComponent as SunSVG } from '../assets/sun.svg';
+
+import SocialSection from "./socials-section";
+import { ThemeContext } from "../contexts/themeContext";
+
+export default function Header({theme}) {
+    const [ drawerState, setDrawerState ] = useState("");
+    const [ headerState, setHeaderState ] = useState("");
+
+    const { darkMode, toggleDarkMode } = React.useContext(ThemeContext);
+
+    var scrollOffset = 0;
+
+    useEffect(() => {
+        window.onscroll = () => {
+            handleScroll();
+        };
+
+        const headerHeight = $(".header").outerHeight();
+
+        $(".header_drawer").css({
+            "height" : `calc(100vh - ${headerHeight}px)`,
+            "top" : `${headerHeight}px`
+        });
+    }, []);
+
+    function toggleDrawerState() {
+        $("html").toggleClass("noscroll");
+        
+        drawerState === 'open' ? setDrawerState('') : setDrawerState('open');
+    }
+  
+    function handleScroll(){
+        var scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        scroll <= 0 ? $(".scrolltotop").removeClass("scrolled") : $(".scrolltotop").addClass("scrolled");
+    
+        if (scroll > scrollOffset) {
+            setHeaderState("scrolled");
+        } 
+        else {
+            setHeaderState("");
+        }
+    
+        scrollOffset = scroll;
+    }
+
+    return(
+        <header className={theme + ' ' + headerState}>
+            <div className={"header " + theme}>
+                <div className="header_logo">
+                    <a href="/"><LogoDarkSVG/><LogoLightSVG/></a>
+                </div>
+                
+                <nav>
+                    <a href="#about">about</a>
+                    <a href="#experience">experience</a>
+                    <a href="#mywork">my work</a>
+                    <a href="#contactme">contact me</a>
+                </nav>
+                
+                <div className="header_resume">
+                    <div>
+                        <a href="/">resume</a>
+                    </div>
+                </div>
+                
+                <div className="header_theme_toggler">
+                    <button className="header_theme_toggler_toggle" onClick={toggleDarkMode}>
+                        <SunSVG/><MoonSVG/>
+                    </button>
+                </div>
+                
+                <div className={"header_hamburger " + drawerState} onClick={() => toggleDrawerState()}>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            
+            <div className={"header_drawer " + theme + " " + drawerState}>
+                <div className="header_drawer_bottom">
+                    <SocialSection theme={theme}/>
+
+                    <div className="header_drawer_bottom_menu">
+                        <nav>
+                            <a href="#about">about</a>
+                            <a href="#experience">experience</a>
+                            <a href="#mywork">my work</a>
+                            <a href="#contactme">contact me</a>
+                        </nav>
+                    
+                        <div className="header_drawer_bottom_menu_resume">
+                            <div>
+                                <a href="/">resume</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
