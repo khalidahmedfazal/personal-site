fe0ffbbb492fd6810bfb438cacc56da2becb7e74
Khalid Fazal <khalidahmedfazal01@gmail.com>
2022-03-13 17:21:53 +0300

    Added animated background to tag cloud

--- src/components/about.js
+++ src/components/about.js
@@ -5,9 +5,20 @@ import TagCloud from "TagCloud";
 
 import { SectionHeading } from "./sectionHeading";
 
+import { ReactComponent as TechPatternSVG } from '../assets/tech-pattern-dark.svg';
+
 // import Portrait from '../assets/portrait.webp';
 
 export default function About({theme}) {
+    var animated = false;
+
+    useEffect(() => {
+        $(document).on('scroll', handleScroll);
+    }, [])
+
+    useEffect(() => {
+        handleScroll();
+    }, [theme])
 
     useEffect(() => {
         $("#tagcloud-techs").empty();
@@ -35,36 +46,25 @@ export default function About({theme}) {
             itemClass: "tagcloud--item technologies " + theme
         };
     
-        TagCloud(".about_content_techs", texts, options);
+        TagCloud("#tagcloud-techs", texts, options);
     }, [window.innerWidth, theme])
-    
+
+    const handleScroll = () => {
+        if(!animated) {
+            var scrollAmount = window.pageYOffset;
+            var contentOffset = document.getElementById("about").getBoundingClientRect().top;
+
+            if(scrollAmount >= contentOffset) {
+                $(`.about_content_techs-wrapper.${theme} > svg > path`).css({ animation: `dash-${theme} 5s forwards ease-in-out` });
+            }
+        }
+    }
 
     return(
         <section className={"about " + theme} id="about">
             <SectionHeading theme={theme} heading="about"/>
 
-            <div className="about_content">
-                <div className="about_content_text">
-                    <p>
-                        Look like you made it here! Well, I'm <em>Khalid</em> and I love creating things that live on the web. I'm a <em>20</em> year old tech geek with a bachelor's degree in Engineering from <em>Staffordshire University</em>, with a focus in <em>Software Engineering</em>.
-                    </p>
-
-                    <p>
-                        While I consider myself a <em>full-stack developer</em>, I have a passion for <em>frontend development</em>. My other areas of interest are <em>game development</em>, <em>IoT</em> and <em>graphic designing</em>. In my free time, I play <em>video games</em>, <em>work out</em> and watch <em>Formula 1</em>. 
-                    </p>
-                    
-                    <p>Here's a cool little thingy of the technologies I've been working with.</p>
-
-                    {/* <ul>
-                        <li>JavaScript (ES6+)</li>
-                        <li>React</li>
-                        <li>Sass</li>
-                        <li>WordPress</li>
-                        <li>ASP.NET</li>
-                        <li>Android</li>
-                    </ul> */}
-                </div>
-
+            <div className="about_content" id="about-content">
                 <div className="about_content_text">
                     <p>
                         Look like you made it here! Well, I'm <em>Khalid</em> and I love creating things
@@ -81,33 +81,22 @@ export default function About({theme}) {
                         <br/>
                         <em>frontend development</em>. My other areas of interest are <em>game</em>
                         <br/>
-                        <em>development</em>, <em>IoT</em> and <em>graphic designing</em>. In my free
+                        <em>development</em>, <em>IoT</em> and <em>graphic designing</em>. While I'm not
                         <br/>
-                        time, I hop on <em>Warzone</em>, <em>work out</em> or watch some 
+                        grinding, I hop on <em>Warzone</em>, <em>work out</em> or watch some 
                         <br/>
                         good old <em>Formula 1</em>. 
                     </p>
                     
                     <p>Here's a cool little thingy of the technologies I've been working with.</p>
-
-                    {/* <ul id="techs">
-                        <li>JavaScript (ES6+)</li>
-                        <li>React</li>
-                        <li>Sass</li>
-                        <li>WordPress</li>
-                        <li>ASP.NET</li>
-                        <li>Android</li>
-                    </ul> */}
-                    {/* <div className="about_content_text_techs"></div> */}
                 </div>
-
-                {/* <div className="about_content_portrait">
-                    <span></span>
-
-                    <img src={Portrait} alt="portrait" />
-                </div> */}
                     
-                <div className="about_content_techs" id="tagcloud-techs"></div>
+                <div className={"about_content_techs-wrapper " + theme} id="tagcloud-techs-wrapper">
+                    <TechPatternSVG/>
+
+                    <div className="about_content_techs" id="tagcloud-techs">
+                    </div>
+                </div>
             </div>
         </section>
     );
